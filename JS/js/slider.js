b88852fd9725f6474b16703d216e85be47c13090
Keith Gerhards <keith.gerhards@gmail.com>
2018-04-15 09:23:58 -0600

    updated text and images

--- JS/js/slider.js
+++ JS/js/slider.js
@@ -22,25 +22,27 @@ function knslider() {
 //catherine.JPG		hunter.jpg		lauraandjohn.jpg	priest.jpg		travis.jpg		wed2.jpg
 
     imgURL.push('images/people/kaylakarl.jpg');
-    imgInfo.push('Kayla and Carl, Nikki\'s Mother and step Father');
+    imgInfo.push('Kayla and Carl, Nikki\'s mother and stepfather');
     imgURL.push('images/people/philandclella.jpg');
-    imgInfo.push('Philip and Clella Barbour Nikki\'s Dad and step Mother.');
+    imgInfo.push('Philip and Clella, Nikki\'s dad and stepmother');
     imgURL.push('images/people/momanddad.jpg');
-    imgInfo.push('Muriel and Karl Gerhards Keith\'s parents');
+    imgInfo.push('Muriel and Karl, Keith\'s parents');
     imgURL.push('images/people/torri.jpg');
-    imgInfo.push('Tori Hurni, Nikki\'s Sister');
+    imgInfo.push('Tori Hurni, Nikki\'s sister and bridesmaid');
     imgURL.push('images/people/priest.jpg');
     imgInfo.push('Andrew Gerhards, Keith\'s nephew and officiant');
     imgURL.push('images/people/alex.jpg');
-    imgInfo.push('Alex Gerhards, Keith\'s Son and groomsman');
+    imgInfo.push('Alex Gerhards, Keith\'s son and groomsman');
     imgURL.push('images/people/travis.jpg');
-    imgInfo.push('Travis Gerhards, Keith\'s Son and groomsman');
+    imgInfo.push('Travis Gerhards, Keith\'s son and groomsman');
     imgURL.push('images/people/graceandas.jpg');
-    imgInfo.push('Grace Gerhards and Austin Scott, Keith\'s daughter. Maid of honor and best man.');
+    imgInfo.push('Grace Gerhards and Austin Scott, Keith\'s daughter and maid of honor and best man');
     imgURL.push('images/people/ummers.jpg');
-    imgInfo.push('Aubrey Durand Nikki\'s sister and bridesmaid');
+    imgInfo.push('Aubrey Durand, Nikki\'s sister and bridesmaid');
     imgURL.push('images/people/shanna.jpg');
-    imgInfo.push('Shanna Martin Nikki\'s friend and bridesmaid');
+    imgInfo.push('Shanna Martin, Nikki\'s childhood friend and bridesmaid');
+    imgURL.push('images/people/kara.jpg');
+    imgInfo.push('Kara Nipp, Nikki\'s childhood friend and bridesmaid');
 
     var pstyle = "<p style='font-family:Brush Script Std, cursive;font-weight:bold;font-size:2.0em;margin: 0;padding: 0 auto;text-align:center'>";
 
